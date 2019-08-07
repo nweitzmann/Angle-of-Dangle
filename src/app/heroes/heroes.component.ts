53ffb0c14824de4d2b7d2214115b09b8a873322c
Noah <noah@booksbnb.com>
2019-08-07 16:01:26 -0500

    Adam's changes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-
+import { HEROES } from '../mock-heroes';
 
 
 @Component({
@@ -9,17 +9,17 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
+  heroes: Hero[] = HEROES;
 
 
-  };
+
+
+
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() {console.log(this.heroes)
   }
-
-
 }
+
+
